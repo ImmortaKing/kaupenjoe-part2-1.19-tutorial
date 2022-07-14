ef7b2110367ba42278a3cca5ba91e487ff8a39d4
Immorta King <108091277+ImmortaKing@users.noreply.github.com>
2022-07-14 16:58:33 -0400

    Initial commit

---
+++ src/main/java/net/coolkidsforsale/tutorialmod/TutorialMod.java
@@ -0,0 +1,19 @@
+package net.coolkidsforsale.tutorialmod;
+
+import net.coolkidsforsale.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very Important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+
+	}
+}
