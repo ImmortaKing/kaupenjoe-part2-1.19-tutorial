6ae123afd544bbdf540bd87e97f6a3e97370ac09
Immorta King <108091277+ImmortaKing@users.noreply.github.com>
2022-07-15 12:48:11 -0400

    part 8

--- src/main/java/net/coolkidsforsale/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/coolkidsforsale/tutorialmod/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.coolkidsforsale.tutorialmod.block;
 
 import net.coolkidsforsale.tutorialmod.TutorialMod;
 import net.coolkidsforsale.tutorialmod.block.custom.JumpyBlock;
+import net.coolkidsforsale.tutorialmod.block.custom.TanzaniteLampBlock;
 import net.coolkidsforsale.tutorialmod.item.ModItemGroup;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -32,10 +33,12 @@ public class ModBlocks {
             new OreBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool(),
                     UniformIntProvider.create(3, 7)), ModItemGroup.TANZANITE);
 
-
-
     public static final Block JUMPY_BLOCK = registerBlock("jumpy_block",
             new JumpyBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.TANZANITE);
+
+    public static final Block TANZANITE_LAMP = registerBlock("tanzanite_lamp",
+            new TanzaniteLampBlock(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()
+                    .luminance(state -> state.get(TanzaniteLampBlock.LIT) ? 15 : 0)), ModItemGroup.TANZANITE);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
