1f803a6380b3aee96565c18091554bcd89ce82ba
Immorta King <108091277+ImmortaKing@users.noreply.github.com>
2022-07-26 18:23:15 -0400

    part 14

--- src/main/java/net/coolkidsforsale/tutorialmod/TutorialModClient.java
+++ src/main/java/net/coolkidsforsale/tutorialmod/TutorialModClient.java
@@ -1,6 +1,7 @@
 package net.coolkidsforsale.tutorialmod;
 
 import net.coolkidsforsale.tutorialmod.block.ModBlocks;
+import net.coolkidsforsale.tutorialmod.event.KeyInputHandler;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
@@ -9,5 +10,7 @@ public class TutorialModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.EGGPLANT_CROP, RenderLayer.getCutout());
+
+        KeyInputHandler.register();
     }
 }
